79f54c55ee90e9ce1d509c45d49cc944203b1bc7
Collins Mathews <cmat0007@student.monash.edu>
2020-09-23 13:38:52 +1000

    WIP Chat Page

--- client/src/components/chatPage.js
+++ client/src/components/chatPage.js
@@ -2,54 +2,56 @@ import React, { useState, useEffect, useRef, Component } from 'react';
 import './styles/App.css';
 import Talk from 'talkjs';
 
+class InboxApp extends React.Component {
+	constructor(props) {
+		super(props)
+		this.talkjsContainer = React.createRef();
+	}
+
+	componentDidMount() {
+		Talk.ready.then(() => {
+			var me = new Talk.User({
+				id: parseInt(Math.random() * 500000).toString(),
+				name: "Alice",
+				email: "demo@talkjs.com",
+				photoUrl: "https://demo.talkjs.com/img/alice.jpg",
+				welcomeMessage: "Hey there! How are you? :-)",
+				role: "booker"
+			});
+
+			window.talkSession = new Talk.Session({
+				appId: "Hku1c4Pt",
+				me: me
+			});
+
+			var other = new Talk.User({
+				id: parseInt(Math.random() * 500000).toString(),
+				name: "Sebastian",
+				email: "demo@talkjs.com",
+				photoUrl: "https://demo.talkjs.com/img/sebastian.jpg",
+				welcomeMessage: "Hey, how can I help?",
+				role: "booker"
+			});
+
+			var conversation = talkSession.getOrCreateConversation(Talk.oneOnOneId(me, other));
+			conversation.setParticipant(me);
+			conversation.setParticipant(other);
+
+			var inbox = talkSession.createInbox({ selected: conversation });
+			inbox.mount(this.talkjsContainer.current);
+		});
+	}
+
+	render() {
+		return (
+			<div ref={this.talkjsContainer} className="chatbox-container"></div>
+		)
+	}
+}
+
+ReactDOM.render(<InboxApp />, document.querySelector("#app"))
+
 function ChatPage() {
-
-	(function (t, a, l, k, j, s) {
-		s = a.createElement('script'); s.async = 1; s.src = "https://cdn.talkjs.com/talk.js"; a.head.appendChild(s)
-			; k = t.Promise; t.Talk = {
-				v: 3, ready: {
-					then: function (f) {
-						if (k) return new k(function (r, e) { l.push([f, r, e]) }); l
-							.push([f])
-					}, catch: function () { return k && new k() }, c: l
-				}
-			};
-	})(window, document, []);
-
-	await Talk.ready;
-	const me = new Talk.User({
-		id: "123456",
-		name: "Alice",
-		email: "alice@example.com"
-	});
-	const session = new Talk.Session({
-		appId: "YOUR_APP_ID",
-		me: me
-	});
-
-	const other1 = new Talk.User({
-		id: "654321",
-		name: "Sebastian",
-		email: "sebastian@example.com"
-	});
-
-	const other2 = new Talk.User({
-		id: "456789",
-		name: "Steve",
-		email: "steve@example.com"
-	});
-
-	const conversation = session.getOrCreateConversation("CONVERSATION_ID");
-	conversation.setParticipant(me);
-	conversation.setParticipant(other1);
-	conversation.setParticipant(other2);
-	conversation.setAttributes({
-		photoUrl: "https://demo.talkjs.com/img/11.jpg",
-		subject: "Beautiful Steel Preserve for rent!"
-	});
-
-	var chatbox = session.createChatbox(conversation);
-	chatbox.mount(document.getElementById("talkjs-container"));
 	return (
 		<div>
 			<div id="talkjs-container" styles="width: 90%; margin: 30px; height: 500px">
